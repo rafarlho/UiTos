00b7f5a01900f288f088bc76d96341f3cc05b022
Rafael Carvalho <raffacarvalho2000@gmail.com>
2025-10-26 22:39:51 +0000

    Created project and fixed themes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideHttpClient(), 
+    provideTranslateService({
+        loader: provideTranslateHttpLoader({
+            prefix: './i18n/',
+            suffix: '.json'
+        })
+    })
+            
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TranslateService } from '@ngx-translate/core';
+import { MatButtonModule } from "@angular/material/button"
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('UiTos');
+
+    private translateService = inject(TranslateService)
+
+    constructor() {
+        this.translateService.addLangs(["pt"])
+        this.translateService.setFallbackLang('pt');
+        this.translateService.use('pt');
+    }
 }
