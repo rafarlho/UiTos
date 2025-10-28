86f262d0e2105dd345f8be22d8fbdd3c761f08e1
Rafael Carvalho <raffacarvalho2000@gmail.com>
2025-10-28 23:19:36 +0000

    Implemented authentication

--- src/app/shared/components/login/login.ts
+++ src/app/shared/components/login/login.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { NgxBackgroundBeamsComponent } from "@omnedia/ngx-background-beams";
 import { NgxNeonUnderlineComponent } from '@omnedia/ngx-neon-underline';
 import { NgxFadeComponent } from '@omnedia/ngx-fade';
 import { NgxHighlighterComponent } from "@omnedia/ngx-highlighter";
+import { Auth0ClientService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-login',
@@ -18,5 +19,11 @@ import { NgxHighlighterComponent } from "@omnedia/ngx-highlighter";
   styleUrl: './login.scss',
 })
 export class Login {
-
+  private readonly auth = inject(Auth0ClientService)
+  handleLoginClick() {
+    this.auth.loginWithPopup().then(()=> {
+      console.log(this.auth.getUser().then((data)=> console.log(data)) )
+    })
+    .catch((reason: any) => {console.log(reason)})
+  }
 }
